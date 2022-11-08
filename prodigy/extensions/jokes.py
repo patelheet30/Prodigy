7bfc6340442e07110dc9c2e01f2e606874a3b186
patelheet30 <heetkpatel30@gmail.com>
2022-11-08 12:14:11 +0000

    Finishes work on the joke command

--- prodigy/extensions/jokes.py
+++ prodigy/extensions/jokes.py
@@ -2,7 +2,25 @@ import hikari
 import lightbulb
 import aiohttp
 
-jokes = lightbulb.Plugin("Jokes", "A set of commands which send jokes. Customisable to include blacklists")
+jokes = lightbulb.Plugin("Jokes", "A set of commands which send jokes.")
+
+@jokes.command
+@lightbulb.command("joke", "Sends a joke")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def on_joke(ctx: lightbulb.SlashContext) -> None:
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://some-random-api.ml/joke") as jsondata:
+            data = await jsondata.json()
+        
+    joke = data['joke']
+    joke_embed = (
+        hikari.Embed(
+            title="Joke incoming!!!",
+            description=joke
+        )
+        .set_author(name=f"{ctx.author}", icon=f"{ctx.author.avatar_url}")
+    )
+    await ctx.respond(joke_embed)
 
 def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(jokes)
